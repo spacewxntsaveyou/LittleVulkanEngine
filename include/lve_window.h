9ddbefe6990eece78752e58000dd9bfc8a07d03d
Crashy Crashed <crashy.crashed@gmail.com>
2026-01-01 22:45:06 +0100

    Deleting copy constructor

--- include/lve_window.h
+++ include/lve_window.h
@@ -16,6 +16,9 @@ namespace lve {
 		LveWindow(int w, int h, std::string name);
 		~LveWindow();
 
+		LveWindow(const LveWindow&) = delete;
+		LveWindow& operator=(const LveWindow&) = delete;
+
 		bool shouldClose() { return glfwWindowShouldClose(window); }
 
 	private:
