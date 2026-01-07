4883ed8776ed0d6e3036083ac48691cc63aea23a
Crashy Crashed <crashy.crashed@gmail.com>
2026-01-07 17:21:33 +0100

    Tied Vertex and Fragment shaders together

--- src/lve_model.cpp
+++ src/lve_model.cpp
@@ -56,11 +56,18 @@ namespace lve {
 
 	std::vector<VkVertexInputAttributeDescription> LveModel::Vertex::getAttributeDescriptions() {
 
-		std::vector<VkVertexInputAttributeDescription> attributeDescriptions(1);
+		std::vector<VkVertexInputAttributeDescription> attributeDescriptions(2);
 		attributeDescriptions[0].binding = 0;
 		attributeDescriptions[0].location = 0;
 		attributeDescriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
-		attributeDescriptions[0].offset = 0;
+		attributeDescriptions[0].offset = offsetof(Vertex, position);
+		//could be {0, 0, VK_FORMAT, offsetof(Vertex, position)}
+
+		attributeDescriptions[1].binding = 0;
+		attributeDescriptions[1].location = 1;
+		attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+		attributeDescriptions[1].offset = offsetof(Vertex, color);
+		//could be {1, 0, VK_FORMAT, offsetof(Vertex, color)}
 
 		return attributeDescriptions;
 	}
