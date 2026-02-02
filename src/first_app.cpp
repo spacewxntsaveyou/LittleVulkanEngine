6f699890b03c94e63d9ac3ca7a16dc2b9e71e923
Crashy Crashed <crashy.crashed@gmail.com>
2026-02-02 01:41:39 +0100

    Switched to perspective projection

--- src/first_app.cpp
+++ src/first_app.cpp
@@ -29,8 +29,8 @@ namespace lve {
 			glfwPollEvents();
 
             float aspect = lveRenderer.getAspectRatio();
-            camera.setOrthographicProjection(-aspect, aspect, -1, 1, -1, 1);
-
+            //camera.setOrthographicProjection(-aspect, aspect, -1, 1, -1, 1);          //Orthographic projection
+            camera.setPerspectiveProjection(glm::radians(50.f), aspect, 0.1f, 10.f);    //Perspective projection
 			
 			if (auto commandBuffer = lveRenderer.beginFrame()) {
 
@@ -112,7 +112,7 @@ namespace lve {
         std::shared_ptr<LveModel> lveModel = createCubeModel(lveDevice, {.0f, .0f, .0f});
         auto cube = LveGameObject::createGameObject();
         cube.model = lveModel;
-        cube.transform.translation = { .0f, .0f, .5f };
+        cube.transform.translation = { .0f, .0f, 2.5f };
         cube.transform.scale = { .5f, .5f, .5f };
         gameObjects.push_back(std::move(cube));
 
