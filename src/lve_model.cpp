4906abfc3807b0d53d4ab8cc22270b77f929e260
Crashy Crashed <crashy.crashed@gmail.com>
2026-02-04 17:12:22 +0100

    Optimized getAttributeDescriptions()

--- src/lve_model.cpp
+++ src/lve_model.cpp
@@ -139,17 +139,11 @@ namespace lve {
 	std::vector<VkVertexInputAttributeDescription> LveModel::Vertex::getAttributeDescriptions() {
 
 		std::vector<VkVertexInputAttributeDescription> attributeDescriptions(2);
-		attributeDescriptions[0].binding = 0;
-		attributeDescriptions[0].location = 0;
-		attributeDescriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
-		attributeDescriptions[0].offset = offsetof(Vertex, position);
-		//could be {0, 0, VK_FORMAT, offsetof(Vertex, position)}
 
-		attributeDescriptions[1].binding = 0;
-		attributeDescriptions[1].location = 1;
-		attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
-		attributeDescriptions[1].offset = offsetof(Vertex, color);
-		//could be {1, 0, VK_FORMAT, offsetof(Vertex, color)}
+		attributeDescriptions.push_back({ 0, 0, VK_FORMAT_R32G32B32_SFLOAT, offsetof(Vertex, position) });
+		attributeDescriptions.push_back({ 1, 0, VK_FORMAT_R32G32B32_SFLOAT, offsetof(Vertex, color) });
+		attributeDescriptions.push_back({ 2, 0, VK_FORMAT_R32G32B32_SFLOAT, offsetof(Vertex, normal) });
+		attributeDescriptions.push_back({ 3, 0, VK_FORMAT_R32G32_SFLOAT, offsetof(Vertex, uv) });
 
 		return attributeDescriptions;
 	}
