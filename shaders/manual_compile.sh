b91e31f533f853c92a2b47e8d6025e7fc2d8b481
crash <crashy.crashed@gmail.com>
2026-04-20 11:39:43 +0200

    Changed thirdparty to include .versions - Ressucitated manual_compile.sh -

---
+++ shaders/manual_compile.sh
@@ -0,0 +1,7 @@
+D:\2_Foundation\1_Libraries\0_VulkanSDK\Bin\glslc.exe simple_shader.vert -o D:\3_Projects\LittleVulkanEngine\out\build\x64-Debug\shaders\simple_shader.vert.spv
+D:\2_Foundation\1_Libraries\0_VulkanSDK\Bin\glslc.exe simple_shader.frag -o D:\3_Projects\LittleVulkanEngine\out\build\x64-Debug\shaders\simple_shader.frag.spv
+
+D:\2_Foundation\1_Libraries\0_VulkanSDK\Bin\glslc.exe point_light.vert -o D:\3_Projects\LittleVulkanEngine\out\build\x64-Debug\shaders\point_light.vert.spv
+D:\2_Foundation\1_Libraries\0_VulkanSDK\Bin\glslc.exe point_light.frag -o D:\3_Projects\LittleVulkanEngine\out\build\x64-Debug\shaders\point_light.frag.spv
+
+pause
